6740da3111bec796afb166331f28eefc8360de3a
jiaofuming <jiaofuming@iautos.cn>
2018-09-04 17:27:56 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-/* $(".QR-code").on("mouseenter", function() {
-    $(this).addClass("active")
-    $(this).children("div").stop(true).fadeIn(150);
-})
-$(".QR-code").on("mouseleave", function() {
-    $(this).removeClass("active")
-    $(this).children("div").stop(true).fadeOut(150);
-})
-$(".my-collection, .browser-history").on("mouseenter", function() {
-    $(this).addClass("active")
-    $(this).children("div").show().stop(true).animate({
-        width: "75px"
-    }, 150);
-})
-$(".my-collection, .browser-history").on("mouseleave", function() {
-    var this_ = this;
-    $(this).children("div").stop(true).animate({
-        width: "0"
-    }, 150, function() {
-        $(this).hide();
-        $(this_).removeClass("active")
-    });
-}) */
-
 $(".float-box").on("mouseenter", "div", function() {
     var className = $(this).attr("class");
     className = className ? className.split(" ")[0] : "";
@@ -56,7 +32,6 @@ $(".float-box").on("mouseleave", "div", function() {
         });
     }
 })
-
 $(".back-top").on("click", function() {
     $("html, body").animate({
         "scrollTop": "0"
